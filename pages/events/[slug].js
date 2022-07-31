972b2dad9617d444b9effcbfa412e5536ef71de3
Pvag <paolondon@gmail.com>
2022-07-31 12:57:35 +0200

    Layout, Header, Footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,10 @@
+import Layout from "../../components/Layout"
+
 // loads when URL ends with event-1, event-2 or similar, e.g.
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   )
 }
